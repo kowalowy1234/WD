a314cff449a2ce083357323ce3bab1183750b9c2
kowalowy1234 <58771445+kowalowy1234@users.noreply.github.com>
2020-04-08 14:01:53 +0200

    Add files via upload

---
+++ Lista 3/ciagi/geometr.py
@@ -0,0 +1,12 @@
+import math
+
+def wyraz(a1, q, n):
+    an=a1*pow(q, n-1)
+    return a1
+
+def sum(a1, q, n)
+    if(q==1):
+        sum=a1*n
+    else:
+        sum=a1*((1-pow(q, n))/(1-q))
+    return sum
