6757f3c1550efc753729cd803782432b00a9de1c
kowalowy1234 <58771445+kowalowy1234@users.noreply.github.com>
2020-04-22 16:59:59 +0200

    Add files via upload

---
+++ lista 5/zadanie_6.py
@@ -0,0 +1,20 @@
+class Wspak:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+wyraz=Wspak("iksde")
+print(wyraz.__next__())
+print(wyraz.__next__())
+print(wyraz.__next__())
+print(wyraz.__next__())
+print(wyraz.__next__())
